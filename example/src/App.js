9618660fc06209a01f78fab59c8b606cbabca87e
AnirudhSA <saanirud99@gmail.com>
2020-12-16 19:12:09 +0530

    Added easing functions

--- example/src/App.js
+++ example/src/App.js
@@ -5,11 +5,11 @@ import 'react-anim-text/dist/index.css'
 
 const text = "Hello";
 const textList = ["Animated"];
+const easings = ["easeInBounce","easeInCubic","easeOutBounce"];
 const App = () => {
   return(
     <div>
-    
-    <GrowText textList = {textList} duration={1}/>
+    <GrowText textList = {textList} duration={1} easings={easings}/>
     </div>
     );
 }
